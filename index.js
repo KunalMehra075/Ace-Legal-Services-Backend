dd0795672f9dbd7be9335daae2f56def415e54e7
Kunal Mehra <112753481+KunalMehra075@users.noreply.github.com>
2023-04-25 18:56:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json())
 app.use(cors({
   origin: "https://acelegalservices.vercel.app",
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-  allowedHeaders: ['Content-Type', "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"],
+  allowedHeaders: [ 'Access-Control-Allow-Origin','Content-Type', 'Authorization', 'Access-Control-Allow-Credentials'],
   credentials: true
 }));
 
