9437a26f27a006812cf811c3c4373fd92162ab73
KunalMehra075 <112753481+KunalMehra075@users.noreply.github.com>
2023-04-25 22:42:46 +0530

    FinallyErrorFixed

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT;
 //=============> MIDDLEWARES
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "https://acelegalservices.vercel.app",
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', "Authorization", "Access-Control-Allow-Credentials", "Access-Control-Allow-Origin"],
   credentials: true
